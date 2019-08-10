0641fb320dac9a80275186c68ca09bb362ba0f1b
Benji Levine <benjilev08@gmail.com>
2019-08-10 14:20:30 +0100

    Fix Python requests issues

--- dl_location_files.py
+++ dl_location_files.py
@@ -6,25 +6,40 @@ import requests
 
 @click.command()
 def main():
-    with open("/mnt/c/Users/Benjamin/Desktop/FourEels/Houses/Moving Out/" + \
-              "Location Files/location_list", "r") as location_list:
+    #location_files = "/mnt/c/Users/Benjamin/Desktop/FourEels/Houses/" + \
+    #       "Moving Out/Location Files/"
+    #location_files = "/media/benji/OS/Users/Benjamin/Desktop/FourEels/" + \
+    #        "Houses/Moving Out/Location Files/"
+    location_files = "/home/benji/Documents/HouseScrape/Location Files/"
+    with open(f"{location_files}location_list", "r") as location_list:
         for location in location_list:
             location = location.replace(" ", "_").lower().strip()
-            url = "https://www.home.co.uk/for_rent/{l}/current_rents?location={l}".format(l=location)
-            print(url)
+            url = f"https://www.home.co.uk/for_rent/{location}/current_rents?location={location}"
             click.echo("Downloading {} file...".format(location))
-            r = requests.get(url)
-            click.echo("Downloaded {} file.".format(location))
-            click.echo("Writing {} file...".format(location))
-            with open("/mnt/c/Users/Benjamin/Desktop/FourEels/Houses/Moving "+ \
-                      "Out/Location Files/{}".format(location), "w") as loc_file:
-                loc_file.write(r.text)
-            click.echo("Written {} file.")
-            #print(r.text)
+            payload = {"location" : location}
+            headers = {
+                #"Host" : "www.home.co.uk",
+                #"Connection" : "keep-alive",
+                "DNT" : "1",
+                'User-Agent': 'My User Agent 1.0',
+                #'From': 'youremail@domain.com'
+            }
+            r = requests.request(
+                    method="get",
+                    url=url,
+                    #params=payload,
+                    allow_redirects=False,
+                    headers=headers
+            )
+            if r.ok:
+                click.echo(f"Downloaded {location} file.")
+                click.echo(f"Writing {location_files}{location}.html file...")
+                with open(f"{location_files}{location}.html", "w") as loc_file:
+                    loc_file.write(r.text)
+                click.echo(f"Written {location} file.")
+            else:
+                click.echo(f"Request returned {r.status_code}")
             exit(0)
-            #print(json.loads(r.json))
-            #for ionkjjin r.json:
-                #print(i)
 
 
 if __name__ == '__main__':
