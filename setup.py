c9789cc731de9726329ee342d4b8c369c16d63b2
Benji Levine <benji@benjilevine.com>
2019-08-29 14:07:57 +0100

    Correct package name and version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='flaskr',
-    version='1.0.0',
+    name='HouseScrape',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
