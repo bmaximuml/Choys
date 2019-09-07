44934fd27cc5e92cc7ef429df6e9d2f8d313dca5
Benji Levine <benji@benjilevine.com>
2019-09-08 00:57:08 +0100

    Remove conftest comments
    rebase

--- tests/conftest.py
+++ tests/conftest.py
@@ -94,8 +94,6 @@ def client(app):
     app.config['TESTING'] = True
 
     with app.test_client() as client:
-        # with app.app_context():
-        #     flaskr.init_db()
         yield client
 
     os.close(db_fd)
