77dfa9b4a7a32b6eba823c5a9a5b52c6080763be
Benji Levine <benji@benjilevine.com>
2020-03-23 16:01:52 +0000

    Apply filters from GET data

--- flaskr/compare_locations.py
+++ flaskr/compare_locations.py
@@ -10,9 +10,46 @@ bp = Blueprint('compare_locations', __name__)
 
 @bp.route('/')
 def index():
+    categories = [
+        'name',
+        'total_properties',
+        'average_rent',
+        'rent_under_250',
+        'rent_250_to_500',
+        'distance_to_london',
+        'duration_to_london',
+        'score'
+    ]
+
     logger = logging.getLogger()
 
     all_locations = get_location_data()
+
+    for location in all_locations:
+        for category in categories:
+            if request.args.get(category) is None:
+                # No value set for category, skip to next category
+                continue
+
+            category_switch = request.args.get(f'{category}_switch', default='or-less')
+
+            try:
+                location_category = float(location[category])
+                request_category = float(request.args.get(category))
+            except ValueError:
+                location_category = str(location[category])
+                request_category = str(request.args.get(category))
+
+            if ((
+                    category_switch == 'or-more' and
+                    location_category < request_category
+                ) or (
+                    category_switch == 'or-less' and
+                    location_category > request_category
+            )):
+                all_locations.remove(location)
+                # If location is removed, don't check remaining categories
+                break
 
     sort = request.args.get('sort', default='name')
     sort_desc = request.args.get('dir', default='asc') == 'desc'
