a38ebe96080a4ba68a5e7b420763c7d59d7a33dc
Benji Levine <benji@benjilevine.com>
2020-03-23 14:42:54 +0000

    Implement sorting with GET params

--- flaskr/compare_locations.py
+++ flaskr/compare_locations.py
@@ -1,7 +1,7 @@
 import logging
 
 from datetime import datetime
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from . import get_location_data
 
@@ -14,9 +14,14 @@ def index():
 
     all_locations = get_location_data()
 
+    sort = request.args.get('sort', default='name')
+    sort_desc = request.args.get('dir', default='asc') == 'desc'
+
     logger.info(f'Total Locations: {len(all_locations)}')
     return render_template(
         'compare_locations.html',
         location_array=all_locations,
-        year=datetime.now().year
+        year=datetime.now().year,
+        sort=sort,
+        sort_desc=sort_desc
     )
